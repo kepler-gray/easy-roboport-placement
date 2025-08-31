fc99d1e9062bf6b26163dd23c04c7c2b8bfd3d31
kepler-gray <116854308+kepler-gray@users.noreply.github.com>
2025-08-31 03:35:23 -0700

    Update control.lua
    Replaced all instances of event.created_entity with event.entity.

--- control.lua
+++ control.lua
@@ -1,7 +1,7 @@
 script.on_event(defines.events.on_built_entity, function(event)
     local setting_value = settings.get_player_settings(event.player_index)["draw-roboport-ghost-range"]
-    if setting_value.value and event.created_entity.type == "entity-ghost" and event.created_entity.ghost_type == "roboport" then
-        local roboportghost = event.created_entity
+    if setting_value.value and event.entity.type == "entity-ghost" and event.entity.ghost_type == "roboport" then
+        local roboportghost = event.entity
         local player = game.players[event.player_index]
         local col = { r = 0.2, g = 0.1, b = 0.2, a = 0.01 }
         local rad = game.entity_prototypes[roboportghost.ghost_name].logistic_radius
@@ -24,3 +24,4 @@ script.on_event("toggle-ghost-logistic-range", function(event)
     settings.get_player_settings(event.player_index)["draw-roboport-ghost-range"] = {
         value = not settings.get_player_settings(event.player_index)["draw-roboport-ghost-range"].value }
 end)
+
